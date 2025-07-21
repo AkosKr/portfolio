de7999871f5410b71b3a8763529560a80cd2e4dc
AkosKr <akranics1@gmail.com>
2025-07-21 14:36:04 +0000

    Responsive design

--- src/PortfolioGUI.tsx
+++ src/PortfolioGUI.tsx
@@ -7,12 +7,12 @@ function PortfolioGUI() {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className="h-screen snap-y snap-mandatory overflow-scroll scroll-smooth scrollbar-hide">
-      <div className="sticky h-screen top-0 left-0 h-[0px]">
-        <div className="flex flex-auto justify-around h-[100px] bg-bg items-center">
-          <div></div>
-          <nav className="flex flex-row top-0 place-content-center">
-            <ul className="flex flex-row gap-10 p-10 text-md text-text-primary">
+    <div className="h-screen flex flex-col">
+      <header className="h-[100px] top-0 w-full bg-bg">
+        <div className="flex flex-auto w-full justify-around h-[100px] bg-bg items-center">
+          <div className="w-[20px] h-[20px]"></div>
+          <nav>
+            <ul className="flex flex-row gap-[50px] text-[18px] text-text-primary">
               <li>
                 <a
                   className="hover:border-b-2 hover:border-text-primary"
@@ -41,31 +41,33 @@ function PortfolioGUI() {
           </nav>
           <button className="flex-none" onClick={toggleTheme}>
             {theme === "light" ? (
-              <FontAwesomeIcon icon={faMoon} />
+              <FontAwesomeIcon className="w-[20px] h-[20px]" icon={faMoon} />
             ) : (
-              <FontAwesomeIcon icon={faSun} />
+              <FontAwesomeIcon className="w-[20px] h-[20px]" icon={faSun} />
             )}
           </button>
         </div>
-      </div>
+      </header>
 
-      <div id="about" className="snap-center snap-mandatory h-screen z-55">
-        <About />
-      </div>
-      <div id="skills" className="snap-center snap-mandatory h-screen">
-        <div className="relative min-h-screen flex">
-          <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-4xl ">
-            Skills
+      <main className="flex-1 overflow-y-scroll snap-y snap-mandatory scroll-smooth scrollbar-hide">
+        <div id="about" className="snap-center snap-mandatory h-[110%] md:h-full">
+          <About />
+        </div>
+        <div id="skills" className="snap-center snap-mandatory h-full">
+          <div className="relative min-h-screen flex">
+            <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-4xl ">
+              Skills
+            </div>
           </div>
         </div>
-      </div>
-      <div id="projects" className="snap-center snap-mandatory h-screen">
-        <div className="relative min-h-screen flex">
-          <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-4xl ">
-            Projects
+        <div id="projects" className="snap-center snap-mandatory h-full">
+          <div className="relative min-h-screen flex">
+            <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-4xl ">
+              Projects
+            </div>
           </div>
         </div>
-      </div>
+      </main>
     </div>
   );
 }
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import Gmail from "/gmail.png";
-import Github from "/github-sign.png";
+import Github from "/github.png";
 import Gitlab from "/gitlab.png";
 import Linkedin from "/linkedin.png";
 import { useCallback } from "react";
@@ -7,47 +7,89 @@ import { useCallback } from "react";
 function About() {
   const openGithub = useCallback(() => {
     window.open("https://github.com/AkosKr", "_blank");
-    console.log("Redirecting");
+  }, []);
+
+  const openGitlab = useCallback(() => {
+    window.open("https://gitlab.com/AkosKr", "_blank");
+  }, []);
+
+  const openGmail = useCallback(() => {
+    window.open(
+      "https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSPFqxCnBtpmcTbrZTNqJfktCDVGFLDHfWbTBRWlVdkBqSxlwDRHlpJKgpZBzDgzvBVRXsGX",
+      "_blank",
+    );
+  }, []);
+
+  const openLinkedin = useCallback(() => {
+    window.open("https://github.com/AkosKr", "_blank");
   }, []);
 
   return (
     <>
-      <div className="relative min-h-screen flex">
-        <div className="container max-w-screen-xl mx-auto flex justify-center items-center text-4xl ">
-          <div className="flex flex-row justify-around">
-            <div className="flex flex-col gap-3">
-              <h1 className="text-[64px] font-bold">HI,</h1>
-              <h1 className="text-[64px] font-bold">I'm Akos Kranics</h1>
-              <p className="text-[24px] font-regular">
-                I'm a full-stack developer interested in backend systems and
-                cybersecurity. I write maintainable code, work with modern
-                frameworks, and build applications that prioritize clarity,
-                reliability, and security.
-              </p>
-              <div className="flex flex-row gap-5 p-2">
-                <button
-                  type="button"
-                  onClick={() => {
-                    console.log("CLICKED");
-                    window.open("https://github.com/AkosKr", "_blank");
-                  }}
-                  className="p-0 border-none bg-red-500 relative z-[999]"
-                >
+      <div className="min-h-full flex">
+        <div className="flex max-w-screen-xl mx-auto my-10 md:my-auto h-full">
+          <div className="flex flex-col gap-10 lg:gap-0 lg:flex-row w-full justify-around items-center">
+            <div className="flex flex-col w-[80%] sm:w-[70%] md:w-[70%] lg:w-1/2 h-full justify-around items-center">
+              <div className="flex flex-col gap-5 w-[95%] sm:w-[90%] lg:w-[85%]">
+                <div className="flex flex-col gap-3">
+                  <h1 className="text-[36px] sm:text-[48px] md:text-[52px] xl:text-[64px] leading-none font-bold">HI,</h1>
+                  <h1 className="text-[36px] sm:text-[48px] md:text-[52px] xl:text-[64px] leading-none font-bold">
+                    I'm Akos Kranics
+                  </h1>
+                </div>
+                <p className="text-[16px] sm:text-[20px] md:text-[22px] lg:text-[24px]/8 font-regular xl:text-justify">
+                  I'm a full-stack developer interested in backend systems and
+                  cybersecurity. I write maintainable code, work with modern
+                  frameworks, and build applications that prioritize clarity,
+                  reliability, and security.
+                </p>
+                <div className="flex flex-row gap-5 ">
                   <img
                     src={Github}
+                    onClick={openGithub}
                     alt="GitHub"
-                    className="w-[32px] h-[32px] cursor-pointer z-55 border border-red-500"
+                    className="w-[32px] h-[32px] cursor-pointer rounded-md"
                   />
-                </button>
-                <img src={Gitlab} alt="Gmail" className="w-[32px] h-[32px]" />
-                <img src={Linkedin} alt="Gmail" className="w-[32px] h-[32px]" />
-                <img src={Gmail} alt="Gmail" className="w-[32px] h-[32px]" />
+                  <img
+                    src={Gitlab}
+                    onClick={openGitlab}
+                    alt="Gitlab"
+                    className="w-[32px] h-[32px] cursor-pointer"
+                  />
+                  <img
+                    src={Gmail}
+                    onClick={openGmail}
+                    alt="Gmail"
+                    className="w-[32px] h-[32px] cursor-pointer"
+                  />
+                  <img
+                    src={Linkedin}
+                    onClick={openLinkedin}
+                    alt="LinkedIn"
+                    className="w-[32px] h-[32px] cursor-pointer"
+                  />
+                </div>
               </div>
-              <div>
-                <button type="button" className="border-2 p-2 ">
-                  Resume
-                </button>
+              <div className="flex justify-start items-end w-[95%] sm:w-[90%] lg:w-[85%] h-[70px] lg:h-[100px]">
+                <a
+                  href="/KranicsAkosCV.pdf"
+                  download="Kranics Akos's Resume"
+                  target="_blank"
+                >
+                  <button
+                    type="button"
+                    className="border-2 rounded-xl p-2 bg-text-secondary text-white"
+                  >
+                    Resume
+                  </button>
+                </a>
               </div>
+            </div>
+            <div className="flex w-[70%] lg:w-1/2 place-content-center">
+              <img
+                src="cropped_circle_image.png"
+                className="w-[212px] h-[212px] sm:w-[312px] sm:h-[312px] md:w-[380px] md:h-[380px] xl:w-[425px] xl:h-[425px]"
+              />
             </div>
           </div>
         </div>
